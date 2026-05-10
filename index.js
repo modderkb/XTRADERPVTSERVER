09383a145f6e63ef83cfd10a72f276c409a14fd9
modderkb <modderkb@gmail.com>
2026-05-10 10:05:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 
+‎const TelegramBot = require("node-telegram-bot-api");
+‎
+‎const bot = new TelegramBot("8588316505:AAEh5vsJZ8fCBnb-1Y38EDMlS7lwVS3wVZs", { polling: true });
+‎
+‎bot.onText(/\/start/, (msg) => {
+‎
+‎bot.sendMessage(msg.chat.id, "CLICK BUTTON", {
+‎
+‎reply_markup: {
+‎inline_keyboard: [
+‎[
+‎{
+‎text: "OPEN",
+‎url: "https://xtraderpvtserver.netlify.app/"
+‎}
+‎]
+‎]
+‎}
+‎
+‎});
+‎
+‎});
